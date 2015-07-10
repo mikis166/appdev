3cd9df109bdad4c42b75932fb6219a41bda58e03
mikis dev <michelesaladino@hotmail.com>
2015-07-10 08:51:09 +0200

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
   resources :contacts
   get '/about' => 'pages#about'
-  root 'pages#index'
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
