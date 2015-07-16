137f90ffe7fa51a681ab3e7feb208e84a29ca860
michele <michelesaladino@hotmail.com>
2015-07-16 18:14:13 +0200

    changed route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resources :contacts
   get '/about' => 'pages#about'
 
-  devise_for :users
-  
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
